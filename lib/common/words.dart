0d7732817746f166f5a6f75561e5e7450bffe775
Abdulbasit Said Ibrahim <aibrahim@ubtconsults.se>
2021-12-03 11:45:49 +0100

    more translation words

--- lib/common/words.dart
+++ lib/common/words.dart
@@ -2,4 +2,15 @@ class TranslationConstants {
   TranslationConstants._();
   static const String register = 'Register';
   static const String signin = 'sign in';
+  static const String whatShouldWeCallYou = 'What should we call you?';
+  static const String yourConactAndLoginDetails =
+      'Your contact and login details';
+  static const String mobileOptional = 'Mobile (optional)';
+  static const String emailAddress = 'Email address';
+  static const String password = 'Password';
+  static const String iHaveReadAndUnderstood =
+      'I have read, understood and accept iDonatio’s ';
+  static const String termsOfService = ' Terms of Service ';
+  static const String and = 'and ';
+  static const String privacyPolicy = 'Privacy Policy.';
 }
