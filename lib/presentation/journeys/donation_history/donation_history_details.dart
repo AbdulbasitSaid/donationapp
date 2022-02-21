848afeda51f78882b9c2be5e8b7bbf217db35760
Abdulbasit Said Ibrahim <aibrahim@ubtconsults.se>
2022-02-21 03:20:30 +0100

    completed donation history details

--- lib/presentation/journeys/donation_history/donation_history_details.dart
+++ lib/presentation/journeys/donation_history/donation_history_details.dart
@@ -3,6 +3,7 @@ import 'package:idonatio/data/models/donation_models/donation_history_model.dart
 import 'package:idonatio/presentation/reusables.dart';
 import 'package:idonatio/presentation/widgets/donee_avatar_place_holder.dart';
 import 'package:idonatio/presentation/widgets/labels/level_2_heading.dart';
+import 'package:idonatio/presentation/widgets/labels/level_4_headline.dart';
 import 'package:intl/intl.dart';
 
 import '../../widgets/labels/level_6_headline.dart';
@@ -80,6 +81,86 @@ class DonationHistoryDetialsScreen extends StatelessWidget {
                     text:
                         ' ${DateFormat.yMMMMEEEEd().format(donationHistoryData.createdAt)}, ${DateFormat.jm().format(donationHistoryData.createdAt).toLowerCase()} '),
               ),
+            ),
+            Container(
+              decoration: whiteContainerBackGround(),
+              padding: const EdgeInsets.all(16),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Level4Headline(text: 'Total payment'),
+                    Level4Headline(
+                      text:
+                          '£${donationHistoryData.donationDetails.map((e) => e.amount).toList().reduce((value, element) => value! + element!)! / 100}',
+                    ),
+                  ],
+                ),
+                const Divider(),
+                ...donationHistoryData.donationDetails.map(
+                  (e) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('${e.donationType?.type}'),
+                        Text('£${e.amount! / 100}'),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Anonymous donation?'),
+                      Text(donationHistoryData.isAnonymous ? 'yes' : 'no'),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('GiftAid enabled?'),
+                      Text(donationHistoryData.applyGiftAidToDonation
+                          ? 'yes'
+                          : 'no'),
+                    ],
+                  ),
+                ),
+              ]),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Level6Headline(text: 'Payment method'),
+            ),
+            Container(
+              decoration: whiteContainerBackGround(),
+              padding: const EdgeInsets.all(16),
+              child: Row(children: [
+                const Icon(Icons.payments),
+                const SizedBox(
+                  width: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                        '${donationHistoryData.cardType} **** ${donationHistoryData.cardLastFourDigits}'),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                        'Exp. ${donationHistoryData.expiryMonth}/${donationHistoryData.expiryYear}'),
+                  ],
+                )
+              ]),
             ),
           ],
         )),
