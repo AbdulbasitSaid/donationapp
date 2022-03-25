7ae2ee0f91ad962a202b2ae0b77c166bb5dbf541
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-03-25 23:57:33 +0100

    improved strip calc

--- lib/common/stripe_charges_calculations.dart
+++ lib/common/stripe_charges_calculations.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import '../data/models/fees_model.dart';
 
-double getCharges(
+ChargesResult getCharges(
     {required List<FeeData> feeData,
     required String cardCurrency,
     required double amount}) {
@@ -40,27 +40,55 @@ double getCharges(
   double nonEuropeanCardFeePercentage = feeData
       .firstWhere((element) => element.tag == 'STRIPE_NON_EUROPEAN_CARD_FEE')
       .value;
-  double idonatioFee =
-      feeData.firstWhere((element) => element.tag == 'IDONATIO_FEE').value *
-          amount;
+  double idonatioFee = getIdonatioTransactionFee(
+      donationAmount: amount,
+      stripeDontionRatio:
+          feeData.firstWhere((element) => element.tag == 'IDONATIO_FEE').value);
+  //
   double stripeFixedFee =
       feeData.firstWhere((element) => element.tag == 'STRIPE_FIXED_FEE').value;
   double cardFee = europeanCountries.contains(cardCurrency.toUpperCase())
       ? europeanCardFeePercentage
       : nonEuropeanCardFeePercentage;
   //
-
   double totalCharge = (amount + idonatioFee + stripeFixedFee) / (1 - cardFee);
-  log(amount.toString());
-  log(idonatioFee.toString());
-  log(stripeFixedFee.toString());
-  log(cardFee.toString());
-  double onlyCharges = totalCharge - amount;
-  var spliteCharge = onlyCharges.toString().split('.');
+
+  double charges = totalCharge - amount;
+  var spliteCharge = charges.toString().split('.');
+  log(spliteCharge.toString());
+  var left = spliteCharge[0];
+  var right = spliteCharge[1].split('');
+  String stringCharg = left + '.' + right.elementAt(0) + right.elementAt(1);
+  charges = double.parse(stringCharg);
   // onlyCharges.
-  return onlyCharges;
+  return amount == 0
+      ? ChargesResult(stripeFee: 0.0, totalFee: 0.0, idonationFee: 0.0)
+      : ChargesResult(
+          idonationFee: idonatioFee, stripeFee: idonatioFee, totalFee: charges);
 }
 
-double getTotalFee(double charges, amount) {
-  return charges + amount;
+//
+double getIdonatioTransactionFee(
+    {required double donationAmount, required double stripeDontionRatio}) {
+  return donationAmount == 0 ? 0 : donationAmount * stripeDontionRatio;
+}
+
+//
+double getStripeTransactionFee({
+  required double donationAmount,
+  required double cardFee,
+  required double fixedStripeFee,
+}) {
+  return donationAmount == 0 ? 0 : donationAmount * cardFee + fixedStripeFee;
+}
+
+class ChargesResult {
+  final double stripeFee;
+  final double totalFee;
+  final double idonationFee;
+
+  ChargesResult(
+      {required this.stripeFee,
+      required this.totalFee,
+      required this.idonationFee});
 }
