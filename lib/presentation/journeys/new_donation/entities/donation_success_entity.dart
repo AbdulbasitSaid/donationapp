b7409a62cf35578533a5bd9b2ae47e56d775792e
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-04-06 17:12:33 +0100

    created donation success entity model

---
+++ lib/presentation/journeys/new_donation/entities/donation_success_entity.dart
@@ -0,0 +1,11 @@
+class DonationSuccessEnitity {
+  final double amount;
+  final String nameOfDonee;
+  final double transactionFee;
+  final bool paidCharges;
+  DonationSuccessEnitity({
+    required this.amount,
+    required this.nameOfDonee,
+    required this.transactionFee,required this.paidCharges,
+  });
+}
