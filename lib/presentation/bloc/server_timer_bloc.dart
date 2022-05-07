3708bf16a69816165a2b1c9418b3a63bcf170485
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-07 20:35:11 +0100

    created ServerTimerBloc

---
+++ lib/presentation/bloc/server_timer_bloc.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:idonatio/common/server_ticker.dart';
+import 'package:idonatio/data/data_sources/user_local_datasource.dart';
+import 'package:idonatio/data/data_sources/user_remote_datasource.dart';
+
+part 'server_timer_event.dart';
+part 'server_timer_state.dart';
+
+class ServerTimerBloc extends Bloc<ServerTimerEvent, ServerTimerState> {
+  static const int _duration = 1500;
+  final ServerTicker _ticker;
+  StreamSubscription<int>? _tickerSubscription;
+  final UserLocalDataSource localDataSource;
+  final UserRemoteDataSource userRemoteDataSource;
+  ServerTimerBloc(
+      {required ServerTicker refereshTicker,
+      required this.userRemoteDataSource,
+      required this.localDataSource})
+      : _ticker = refereshTicker,
+        super(const ServerTimerInitial(_duration)) {
+    on<ServerTimerStarted>(_onStarted);
+    on<ServerTimerTicked>(_onTicked);
+  }
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  void _onStarted(ServerTimerStarted event, Emitter<ServerTimerState> emit) {
+    emit(const ServerTimerRunInProgress(_duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: _duration)
+        .listen((duration) => add(ServerTimerTicked(duration: duration)));
+  }
+
+  void _onTicked(ServerTimerTicked event, Emitter<ServerTimerState> emit) {
+    emit(
+      event.duration > 0
+          ? ServerTimerRunInProgress(event.duration)
+          : const ServerTimerRunComplete(),
+    );
+  }
+}
