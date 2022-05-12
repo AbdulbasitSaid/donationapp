6912fd55b8b747da9567566e8fb9ad7052c82ddb
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-12 17:28:25 +0100

    improved logout cubit

--- lib/presentation/journeys/manage_account/cubit/logout_cubit.dart
+++ lib/presentation/journeys/manage_account/cubit/logout_cubit.dart
@@ -2,17 +2,14 @@ import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:idonatio/data/repository/user_repository.dart';
 
 part 'logout_state.dart';
 
 class LogoutCubit extends Cubit<LogoutState> {
-  LogoutCubit(this._authenticationRepository) : super(LogoutInitial());
-  final UserRepository _authenticationRepository;
+  LogoutCubit() : super(LogoutInitial());
   void logoutUser() async {
     emit(LogoutLoading());
     try {
-      _authenticationRepository.logoutUser();
       emit(LogoutSuccessful());
     } catch (e) {
       log(e.toString());
