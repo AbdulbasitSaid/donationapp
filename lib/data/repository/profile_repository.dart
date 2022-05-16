77bb8b0d794bd85601497be2c21241aaf9ee9a12
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-16 18:37:13 +0100

    included SocketException ProfileRepository

--- lib/data/repository/profile_repository.dart
+++ lib/data/repository/profile_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:idonatio/data/data_sources/profile_remote_datasource.dart';
 import 'package:idonatio/data/data_sources/user_local_datasource.dart';
@@ -19,7 +21,7 @@ class ProfileRepository {
     try {
       final user = await userLocalDataSource.getUser();
       final result =
-          await profileRemoteDataSource.updateProfile(user.token, params); 
+          await profileRemoteDataSource.updateProfile(user.token, params);
       //
       await userLocalDataSource.updateUserData(user.copyWith(
         user: result.data.user.copyWith(donor: result.data.user.donor),
@@ -30,7 +32,7 @@ class ProfileRepository {
           AppError(appErrorType: AppErrorType.unProcessableEntity));
     } on BadRequest {
       return const Left(AppError(appErrorType: AppErrorType.badRequest));
-    } on NetworkError {
+    } on SocketException {
       return const Left(AppError(appErrorType: AppErrorType.network));
     } on UnauthorisedException {
       return const Left(AppError(appErrorType: AppErrorType.unauthorized));
