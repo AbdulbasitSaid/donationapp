3a2af4752aace74730b57a44f59f36c2f5fda3fc
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-16 18:35:40 +0100

    included SocketException CountriesRepository

--- lib/data/repository/countries_repository.dart
+++ lib/data/repository/countries_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:idonatio/data/core/unauthorized_exception.dart';
 import 'package:idonatio/data/data_sources/country_remote_datasource.dart';
@@ -15,10 +17,16 @@ class CountriesRepository {
       return Right(result);
     } on BadRequest {
       return const Left(AppError(appErrorType: AppErrorType.badRequest));
+    } on SocketException {
+      return const Left(AppError(appErrorType: AppErrorType.network));
+    } on UnauthorisedException {
+      return const Left(AppError(appErrorType: AppErrorType.unauthorized));
+    } on Forbidden {
+      return const Left(AppError(appErrorType: AppErrorType.forbidden));
+    } on NotFound {
+      return const Left(AppError(appErrorType: AppErrorType.notFound));
     } on InternalServerError {
       return const Left(AppError(appErrorType: AppErrorType.serveError));
-    } on NetworkError {
-      return const Left(AppError(appErrorType: AppErrorType.network));
     } on Exception {
       return const Left(AppError(appErrorType: AppErrorType.unExpected));
     }
