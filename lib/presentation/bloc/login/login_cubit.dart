ad102c5de5f90fb11061a56d43b3e7668993f361
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-20 15:45:30 +0100

    LoginCubit refactor

--- lib/presentation/bloc/login/login_cubit.dart
+++ lib/presentation/bloc/login/login_cubit.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:idonatio/data/repository/user_repository.dart';
 import 'package:idonatio/domain/entities/app_error.dart';
