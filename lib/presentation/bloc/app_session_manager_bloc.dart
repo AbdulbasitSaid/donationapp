ff8eabcc387aed2166f79d476a8121377c2e7855
Abdulbasit <abdulbasitsaeed619@gmail.com>
2022-05-20 15:46:31 +0100

    AppSessionManagerBloc refactor

--- lib/presentation/bloc/app_session_manager_bloc.dart
+++ lib/presentation/bloc/app_session_manager_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:idonatio/data/core/session_ticker.dart';
 
 part 'app_session_manager_event.dart';
