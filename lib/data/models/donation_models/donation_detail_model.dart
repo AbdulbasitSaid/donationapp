98ee3241f429e494738923f2bb051fa52d4fdeca
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-08 19:25:26 +0100

    created DonationDetailModel

---
+++ lib/data/models/donation_models/donation_detail_model.dart
@@ -0,0 +1,25 @@
+import 'package:idonatio/data/models/donation_models/donation_type_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'donation_detail_model.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class DonationDetailModel {
+  DonationDetailModel({
+    required this.id,
+    required this.donationDetailCode,
+    required this.donationId,
+    required this.donationTypeId,
+    required this.amount,
+    required this.donationType,
+  });
+
+  final String id;
+  final String donationDetailCode;
+  final String donationId;
+  final String donationTypeId;
+  final int amount;
+  final DonationTypeModel donationType;
+  factory DonationDetailModel.fromJson(json) =>
+      _$DonationDetailModelFromJson(json);
+  Map<String, dynamic> toJson() => _$DonationDetailModelToJson(this);
+}
