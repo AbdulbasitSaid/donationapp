094ef3928937c5103bdf56ed5e145dd56ad91586
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-08 19:27:25 +0100

    created DonationHistoryDataModel

---
+++ lib/data/models/donation_models/donation_history_data_model.dart
@@ -0,0 +1,44 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'donee_history_datum_model.dart';
+import 'link_model.dart';
+part 'donation_history_data_model.g.dart';
+
+@JsonSerializable(
+  fieldRename: FieldRename.snake,
+  explicitToJson: true,
+)
+class DonationHistoryDataModel {
+  DonationHistoryDataModel({
+    required this.currentPage,
+    required this.data,
+    required this.firstPageUrl,
+    required this.from,
+    required this.lastPage,
+    required this.lastPageUrl,
+    required this.links,
+    required this.nextPageUrl,
+    required this.path,
+    required this.perPage,
+    required this.prevPageUrl,
+    required this.to,
+    required this.total,
+  });
+
+  final int currentPage;
+  final List<DonationHistoryDatumModel> data;
+  final String firstPageUrl;
+  final int from;
+  final int lastPage;
+  final String lastPageUrl;
+  final List<LinkModel> links;
+  final dynamic nextPageUrl;
+  final String path;
+  final int perPage;
+  final dynamic prevPageUrl;
+  final int to;
+  final int total;
+  factory DonationHistoryDataModel.fromJson(json) =>
+      _$DonationHistoryDataModelFromJson(json);
+  Map<String, dynamic> toJson() => _$DonationHistoryDataModelToJson(this);
+}
