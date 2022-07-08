57fca2c2e617c7a60449a608d8c016793ae566d9
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-08 19:25:11 +0100

    refactored CountryModel

--- lib/data/models/donation_models/country_model.dart
+++ lib/data/models/donation_models/country_model.dart
@@ -2,21 +2,21 @@ import 'package:json_annotation/json_annotation.dart';
 part 'country_model.g.dart';
 
 @JsonSerializable(fieldRename: FieldRename.snake)
-class Country {
+class CountryModel {
   String? id;
   String? name;
   String? countryCode;
   String? currencyCode;
   bool? isVisible;
 
-  Country(
+  CountryModel(
       {this.id,
       this.name,
       this.countryCode,
       this.currencyCode,
       this.isVisible});
 
-  factory Country.fromJson(Map<String, dynamic> json) =>
-      _$CountryFromJson(json);
-  Map<String, dynamic> toJson() => _$CountryToJson(this);
+  factory CountryModel.fromJson(Map<String, dynamic> json) =>
+      _$CountryModelFromJson(json);
+  Map<String, dynamic> toJson() => _$CountryModelToJson(this);
 }
