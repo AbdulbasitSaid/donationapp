9c87ff4b9aeda17306f729ad31383dd972efd074
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-08 19:28:49 +0100

    refactored DoneeModel

--- lib/data/models/donation_models/donee_model.dart
+++ lib/data/models/donation_models/donee_model.dart
@@ -2,66 +2,75 @@ import 'package:idonatio/data/models/donation_models/country_model.dart';
 import 'package:idonatio/data/models/donation_models/organization_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import 'donation_type_model.dart';
 part 'donee_model.g.dart';
 
 @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
-class Donee {
-  String? id;
-  String? doneeCode;
-  String? firstName;
-  String? lastName;
-  String? stripeConnectedAccountId;
-  String? dateOfBirth;
-  String? email;
-  bool? isActive;
-  String? countryId;
-  String? type;
-  String? jobTitle;
-  String? addressLine_1;
-  String? addressLine_2;
-  String? city;
-  String? postalCode;
-  String? phoneNumber;
-  String? verifiedAt;
-  DateTime? createdAt;
-  Country? country;
-  Organization? organization;
-  DonationTypeModel? donationTypeModel;
-  Donee(
-      {this.id,
-      this.doneeCode,
-      this.firstName,
-      this.lastName,
-      this.stripeConnectedAccountId,
-      this.dateOfBirth,
-      this.email,
-      this.isActive,
-      this.countryId,
-      this.type,
-      this.jobTitle,
-      this.addressLine_1,
-      this.addressLine_2,
-      this.city,
-      this.postalCode,
-      this.phoneNumber,
-      this.verifiedAt,
-      this.country,
-      this.organization,
-      this.createdAt});
-  String get fullName {
-    return organization == null
-        ? '$firstName $lastName'
-        : '${organization!.name}';
-  }
+class DoneeModel {
+  DoneeModel({
+    required this.id,
+    required this.doneeCode,
+    required this.firstName,
+    required this.lastName,
+    required this.isOnboarded,
+    required this.hasCompletedStripeOnboarding,
+    required this.roleId,
+    required this.organizationId,
+    required this.stripeConnectedAccountId,
+    required this.dateOfBirth,
+    required this.email,
+    required this.isActive,
+    required this.hasVerifiedCharity,
+    required this.feeRate,
+    required this.countryId,
+    required this.type,
+    required this.jobTitle,
+    required this.addressLine1,
+    required this.addressLine2,
+    required this.city,
+    required this.postalCode,
+    required this.phoneNumber,
+    required this.verifiedAt,
+    required this.createdAt,
+    required this.imageUrl,
+    required this.country,
+    required this.organization,
+  });
 
-  String get fullAddress {
-    return organization == null
-        ? '$addressLine_1 $addressLine_2'
-        : '${organization!.addressLine_1} ${organization!.addressLine_2}';
-  }
+  final String id;
+  final String doneeCode;
+  final String firstName;
+  final String lastName;
+  final bool isOnboarded;
+  final bool hasCompletedStripeOnboarding;
+  final int roleId;
+  final String organizationId;
+  final String stripeConnectedAccountId;
+  final DateTime dateOfBirth;
+  final String email;
+  final bool isActive;
+  final int hasVerifiedCharity;
+  final String feeRate;
+  final String countryId;
+  final String type;
+  final String jobTitle;
+  final String addressLine1;
+  final String addressLine2;
+  final String city;
+  final String postalCode;
+  final String phoneNumber;
+  final DateTime verifiedAt;
+  final DateTime createdAt;
+  final String? imageUrl;
+  final CountryModel country;
+  final Organization? organization;
 
-  factory Donee.fromJson(Map<String, dynamic> json) => _$DoneeFromJson(json);
+  factory DoneeModel.fromJson(json) => _$DoneeModelFromJson(json);
 
-  Map<String, dynamic> toJson() => _$DoneeToJson(this);
+  String get fullAddress => organization == null
+      ? "$addressLine1 $addressLine2"
+      : "${organization!.addressLine_1}" "${organization!.addressLine_2}";
+  String get fullName =>
+      organization == null ? "$firstName $lastName" : "${organization?.name}";
+
+  Map<String, dynamic> toJson() => _$DoneeModelToJson(this);
 }
