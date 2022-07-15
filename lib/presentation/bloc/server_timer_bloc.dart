1a308a426aa676f0ba9d45c429c1ba177bbf78be
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-15 23:08:57 +0100

    refactored ServerTimerBloc

--- lib/presentation/bloc/server_timer_bloc.dart
+++ lib/presentation/bloc/server_timer_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:idonatio/common/server_ticker.dart';
 import 'package:idonatio/data/data_sources/user_local_datasource.dart';
