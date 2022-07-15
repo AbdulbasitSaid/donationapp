eae962542a46fa62ad71f3a44d131e0aac021b72
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-15 23:09:16 +0100

    refactored

--- lib/presentation/cubit/referesh_token_cubit.dart
+++ lib/presentation/cubit/referesh_token_cubit.dart
@@ -1,6 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'referesh_token_state.dart';
 
 class RefereshTokenCubit extends Cubit<RefereshTokenState> {
