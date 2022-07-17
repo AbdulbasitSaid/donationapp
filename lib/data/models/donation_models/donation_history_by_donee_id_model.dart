0404728dea5a5e16f119477ac8ecd8643b39c6fa
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-17 22:13:43 +0100

    added pagination data to DonationHistoryByDoneeIdModel

--- lib/data/models/donation_models/donation_history_by_donee_id_model.dart
+++ lib/data/models/donation_models/donation_history_by_donee_id_model.dart
@@ -1,9 +1,13 @@
+import 'package:idonatio/data/models/donation_models/donee_model.dart';
+import 'package:idonatio/data/models/donation_models/link_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import 'donee_model.dart';
 part 'donation_history_by_donee_id_model.g.dart';
 
-@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
 class DonationHistoryByDoneeIdModel {
   DonationHistoryByDoneeIdModel({
     required this.status,
@@ -13,66 +17,144 @@ class DonationHistoryByDoneeIdModel {
 
   String status;
   String message;
-  List<DonationHistoryByDoneeIdData> data;
+  DonationHistoryByDoneeIdData data;
+
   factory DonationHistoryByDoneeIdModel.fromJson(json) =>
       _$DonationHistoryByDoneeIdModelFromJson(json);
   Map<String, dynamic> toJson() => _$DonationHistoryByDoneeIdModelToJson(this);
 }
 
-@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
 class DonationHistoryByDoneeIdData {
   DonationHistoryByDoneeIdData({
+    required this.currentPage,
+    required this.data,
+    required this.firstPageUrl,
+    required this.from,
+    required this.lastPage,
+    required this.lastPageUrl,
+    required this.links,
+    required this.nextPageUrl,
+    required this.path,
+    required this.perPage,
+    required this.prevPageUrl,
+    required this.to,
+    required this.total,
+  });
+
+  int currentPage;
+  List<DonationHistoryByDoneeIdDatum> data;
+  String firstPageUrl;
+  int from;
+  int lastPage;
+  String lastPageUrl;
+  List<LinkModel> links;
+  String? nextPageUrl;
+  String path;
+  int perPage;
+  dynamic prevPageUrl;
+  int to;
+  int total;
+
+  factory DonationHistoryByDoneeIdData.fromJson(json) =>
+      _$DonationHistoryByDoneeIdDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DonationHistoryByDoneeIdDataToJson(this);
+}
+
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
+class DonationHistoryByDoneeIdDatum {
+  DonationHistoryByDoneeIdDatum({
     required this.id,
+    required this.donationCode,
     required this.donorId,
     required this.doneeId,
-    required this.currency,
-    required this.createdAt,
-    required this.donationDetails,
-    required this.donee,
-    required this.rank,
     required this.paidTransactionFee,
     required this.idonatioTransactionFee,
     required this.stripeTransactionFee,
+    required this.donationMethod,
+    required this.stripePaymentIntentId,
+    this.type,
+    required this.donationLocation,
+    required this.currency,
+    required this.isAnonymous,
+    required this.applyGiftAidToDonation,
+    required this.isPlateDonation,
+    this.channel,
+    required this.stripePaymentMethodId,
+    required this.hasRefund,
+    required this.hasClaimedGiftAid,
+    required this.giftAidClaimId,
+    required this.cardLastFourDigits,
+    required this.cardType,
+    required this.expiryMonth,
+    required this.expiryYear,
+    this.note,
+    required this.createdAt,
+    required this.rank,
+    required this.donationDetails,
+    required this.donee,
   });
 
   String id;
+  String donationCode;
   String donorId;
   String doneeId;
-  String currency;
-  DateTime createdAt;
+  bool paidTransactionFee;
   double idonatioTransactionFee;
   double stripeTransactionFee;
-  bool paidTransactionFee;
+  String donationMethod;
+  String stripePaymentIntentId;
+  dynamic type;
+  String donationLocation;
+  String currency;
+  bool isAnonymous;
+  bool applyGiftAidToDonation;
+  bool isPlateDonation;
+  dynamic channel;
+  String stripePaymentMethodId;
+  bool hasRefund;
+  bool? hasClaimedGiftAid;
+  dynamic giftAidClaimId;
+  String cardLastFourDigits;
+  String cardType;
+  String expiryMonth;
+  String expiryYear;
+  dynamic note;
+  DateTime createdAt;
   int rank;
   List<DonationDetail> donationDetails;
   DoneeModel donee;
-  factory DonationHistoryByDoneeIdData.fromJson(json) =>
-      _$DonationHistoryByDoneeIdDataFromJson(json);
-  Map<String, dynamic> toJson() => _$DonationHistoryByDoneeIdDataToJson(this);
-  String get displayDationType {
-    return donationDetails.length > 1
-        ? 'Multiple donation types'
-        : donationDetails.first.donationType.type;
-  }
 
-  double get totalPayment {
-    return paidTransactionFee
-        ? totalAmount + stripeTransactionFee + idonatioTransactionFee
-        : totalAmount;
-  }
+  factory DonationHistoryByDoneeIdDatum.fromJson(json) =>
+      _$DonationHistoryByDoneeIdDatumFromJson(json);
 
-  double get totalAmount {
-    return donationDetails
-        .map((e) => e.amount)
-        .toList()
-        .reduce((value, element) => value + element);
-  }
+  double get totalPayment => donationDetails
+      .map((e) => e.amount)
+      .toList()
+      .reduce((value, element) => value + element);
+
+  String get displayDationType => donationDetails.length > 1
+      ? 'Multiple Donation Types'
+      : donationDetails.first.donationType.type;
+
+  Map<String, dynamic> toJson() => _$DonationHistoryByDoneeIdDatumToJson(this);
 }
 
-@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
 class DonationDetail {
   DonationDetail({
     required this.id,
+    required this.donationDetailCode,
     required this.donationId,
     required this.donationTypeId,
     required this.amount,
@@ -80,6 +162,7 @@ class DonationDetail {
   });
 
   String id;
+  String donationDetailCode;
   String donationId;
   String donationTypeId;
   double amount;
@@ -88,7 +171,10 @@ class DonationDetail {
   Map<String, dynamic> toJson() => _$DonationDetailToJson(this);
 }
 
-@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
 class DonationType {
   DonationType({
     required this.id,
@@ -97,6 +183,7 @@ class DonationType {
     required this.description,
     required this.giftAidEligible,
     required this.isActive,
+    required this.createdAt,
   });
 
   String id;
@@ -105,6 +192,8 @@ class DonationType {
   String description;
   bool giftAidEligible;
   bool isActive;
+  DateTime createdAt;
+
   factory DonationType.fromJson(json) => _$DonationTypeFromJson(json);
   Map<String, dynamic> toJson() => _$DonationTypeToJson(this);
 }
