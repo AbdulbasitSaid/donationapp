28b2ec27e74a4edf33b3d35c7e16bbe5fb420576
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2022-07-26 23:57:33 +0100

    updated DonationHistorySearchState for pagination

--- lib/presentation/journeys/donation_history/bloc/donation_history_search_state.dart
+++ lib/presentation/journeys/donation_history/bloc/donation_history_search_state.dart
@@ -1,22 +1,68 @@
 part of 'donation_history_search_bloc.dart';
 
-abstract class DonationHistorySearchState extends Equatable {
-  const DonationHistorySearchState();
+class DonationHistorySearchState extends Equatable {
+  const DonationHistorySearchState({
+    this.status = DonationHistorySearchedStatus.initial,
+    this.donationHistory = const <DonationHistoryDatumModel>[],
+    this.hasReachedMax = false,
+    this.message = '',
+    this.currentPage = 1,
+    this.nextPageUrl = '',
+    this.searchQuery = '',
+    this.donationCount = 0,
+  });
+
+  final DonationHistorySearchedStatus status;
+  final List<DonationHistoryDatumModel> donationHistory;
+  final bool hasReachedMax;
+  final String message;
+  final int currentPage;
+  final String nextPageUrl;
+  final String searchQuery;
+  final int donationCount;
+  
+  @override
+  List<Object> get props => [
+        status,
+        donationCount,
+        donationHistory,
+        hasReachedMax,
+        message,
+        currentPage,
+        searchQuery,
+      ];
 
   @override
-  List<Object> get props => [];
+  String toString() {
+    return 'DonationHistorySearchState(status: $status, donationHistory: $donationHistory, hasReachedMax: $hasReachedMax, message: $message, currentPage: $currentPage, nextPageUrl: $nextPageUrl, searchQuery: $searchQuery, donationCount: $donationCount)';
+  }
+
+  DonationHistorySearchState copyWith({
+    DonationHistorySearchedStatus? status,
+    List<DonationHistoryDatumModel>? donationHistory,
+    bool? hasReachedMax,
+    String? message,
+    int? currentPage,
+    String? nextPageUrl,
+    String? searchQuery,
+    int? donationCount,
+  }) {
+    return DonationHistorySearchState(
+      status: status ?? this.status,
+      donationHistory: donationHistory ?? this.donationHistory,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      message: message ?? this.message,
+      currentPage: currentPage ?? this.currentPage,
+      nextPageUrl: nextPageUrl ?? this.nextPageUrl,
+      searchQuery: searchQuery ?? this.searchQuery,
+      donationCount: donationCount ?? this.donationCount,
+    );
+  }
 }
 
-class DonationHistorySearchInitial extends DonationHistorySearchState {}
-
-class DonationHistorySearchSuccessful extends DonationHistorySearchState {
-  final DonationHistoryModel donationHistoryModel;
-
-  const DonationHistorySearchSuccessful(this.donationHistoryModel);
-}
-
-class DonationHistorySearchFailed extends DonationHistorySearchState {
-  final String errorMessage;
-
-  const DonationHistorySearchFailed(this.errorMessage);
+enum DonationHistorySearchedStatus {
+  initial,
+  loading,
+  success,
+  failue,
 }
